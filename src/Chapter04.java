692331a4fd9fbd734d0ee65d3ce135454af23e57
oreorange <92431652+oreorange@users.noreply.github.com>
2022-03-12 18:32:36 +0900

    [Finish]基礎5

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "orange";
+		System.out.println(name);
+		name = "star";
+		final int num = 8;
+		System.out.println(name);
+		System.out.println(num);
+	}
+}
