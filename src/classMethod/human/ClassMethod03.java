7e3bc5dbe093e67047410edda90bc1efbc9dbfd0
oreorange <92431652+oreorange@users.noreply.github.com>
2022-03-18 16:29:26 +0900

    [Finish]基礎9

---
+++ src/classMethod/human/ClassMethod03.java
@@ -0,0 +1,17 @@
+package classMethod.human;
+
+public class ClassMethod03 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Human03 yamada = new Human03();
+
+        System.out.println("私の名前は、" + yamada.name + "です。");
+        String profile = yamada.getProfile();
+        System.out.println(profile + "です。");
+
+        yamada.greet("田辺");
+        yamada.greet(null);
+	}
+
+}
